8b442013f0679e719c8ff8b6b6e69ab966a8cb74
PCs <PCs@DESKTOP-8622E0I>
2022-07-29 16:55:03 -0300

    Adicionar arquivos de projeto.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp3.view;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelecionarMenu(object sender, SelectionChangedEventArgs e)
+        {
+
+            int escolha = Menu.SelectedIndex;
+            
+            if (escolha>=0)
+            {
+                switch (escolha)
+                {
+                    case 0: 
+                        telas.Children.Clear(); 
+                        telas.Children.Add(new tela1());
+                      
+                        break;
+
+
+                    case 1:
+                        telas.Children.Clear();
+                        telas.Children.Add(new tela3());
+                        break;
+
+                    case 2:
+                        telas.Children.Clear();
+                        telas.Children.Add(new tela2());
+                        break;
+
+                    case 3:
+                       this.Close();
+                        break;
+
+
+                }
+            }
+
+            
+            
+        }
+    }
+}
---
+++ WpfApp3/telas3.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace WpfApp3
+{
+    internal class telas3 : UIElement
+    {
+    }
+}
